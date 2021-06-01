cda51357be18ba1fc78bc9298964db0906055d94
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2021-06-01 09:59:38 +0900

    ♻️ 複数書いてたボタン・ナビゲーションを共通化

---
+++ src/components/Detail/DetailButton.vue
@@ -0,0 +1,57 @@
+<template>
+  <button class="change__button" @click="clickFunc">
+    <router-link v-if="nowTheme === `questions`" to="/questions">
+      {{ texts[0] }}
+    </router-link>
+    <router-link v-else-if="nowTheme === `archives`" to="/archives">
+      {{ texts[1] }}
+    </router-link>
+    <router-link v-else to="/completes"> {{ texts[2] }} </router-link>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    texts: {
+      type: Array,
+      require: true,
+      validates(value) {
+        return ["お題", "保留", "済み", "削除"].includes(value)
+      },
+    },
+    nowTheme: {
+      type: String,
+      require: true,
+      validates(value) {
+        return ["questions", "archives", "completes"].includes(value)
+      },
+    },
+    clickFunc: {
+      type: Function,
+      require: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.change__button {
+  display: block;
+  width: 100%;
+  border: none;
+  background-color: lightblue;
+  padding: 4px;
+  border-radius: 25px;
+  color: white;
+  font-weight: bold;
+}
+
+a {
+  display: block;
+}
+
+.change__button:hover {
+  cursor: pointer;
+}
+</style>
---
+++ src/components/frames/Navbar/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar">
+    <ul class="nav__lists">
+      <NavItem path="/questions" match="questions" navTitle="お題" />
+      <NavItem path="/archives" match="archives" navTitle="保留" />
+      <NavItem path="/completes" match="completes" navTitle="済み" />
+    </ul>
+  </nav>
+</template>
+
+<script>
+import NavItem from "@/components/frames/Navbar/NavItem"
+
+export default {
+  components: {
+    NavItem,
+  },
+}
+</script>
+
+<style scoped>
+.navbar {
+  width: 100%;
+  position: fixed;
+  top: 4rem;
+}
+
+.nav__lists {
+  height: 2.3rem;
+  display: flex;
+  border-bottom: #69c2c7 1px solid;
+  background-color: white;
+}
+</style>
